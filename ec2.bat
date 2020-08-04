538726bd0f392634b3bb72ccde2972cd25e0f700
ragudy1989 <64380715+ragudy1989@users.noreply.github.com>
2020-08-03 21:18:17 -0400

    Update ec2.bat

--- ec2.bat
+++ ec2.bat
@@ -1,1 +1,1 @@
-aws ec2 run-instances --image-id ami-09d8b5222f2b93bf0 --count 1 --instance-type t2.micro --key-name demo2 --security-groups ragheedsg --region us-east-1
+aws ec2 run-instances --image-id ami-09d8b5222f2b93bf0 --count 1 --instance-type t2.micro --key-name demo3 --security-groups ragheedsg --region us-east-1
